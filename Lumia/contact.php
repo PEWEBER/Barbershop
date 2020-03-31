23fdb0021548838cc14e5dd6d967614e509750c1
zachariahwestover <35404759+zachariahwestover@users.noreply.github.com>
2020-03-31 15:11:29 -0400

    Moved the map

--- Lumia/contact.php
+++ Lumia/contact.php
@@ -134,7 +134,7 @@
             </aside>
           </div>
           <div class="span8">
-            <iframe src="https://www.google.com/maps/embed?pb=!1m10!1m8!1m3!1d10843.046623466622!2d-86.79249823516956!3d36.10184726870931!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sus!4v1572989335337!5m2!1sen!2sus" width="100%" height="380" frameborder="0" style="border:0" allowfullscreen></iframe>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d12894.200558076225!2d-86.80216023514313!3d36.10445276118799!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x7a732296d8189b03!2sJoe&#39;s%20Barber%20Shop!5e0!3m2!1sen!2sus!4v1585678655638!5m2!1sen!2sus" width="100%" height="380" frameborder="0" style="border:0" allowfullscreen></iframe>
 
             <div class="spacer30">
             </div>
