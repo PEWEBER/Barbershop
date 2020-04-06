c719108dc0b53febd508b6fd05031c2756932d26
zachariahwestover <35404759+zachariahwestover@users.noreply.github.com>
2020-04-06 15:42:58 -0400

    updated footer

--- Lumia/contact.php
+++ Lumia/contact.php
@@ -180,7 +180,7 @@
                 <ul>
                   <li><label><strong>Phone : </strong></label>
                     <p>
-                      +615 966 5042 ext. 5042
+                      (615) 966-5082 ext. 5082
                     </p>
                   </li>
                   <li><label><strong>Email : </strong></label>
@@ -190,7 +190,7 @@
                   </li>
                   <li><label><strong>Address : </strong></label>
                     <p>
-                      One University Park Drive-ish<br>
+                      420 Lineup Ln.<br>
 					  Nashville, TN 37204
                     </p>
                   </li>
@@ -246,58 +246,58 @@
     <!-- Footer
  ================================================== -->
     <footer class="footer">
-      <div class="container">
-        <div class="row">
-          <div class="span4">
-            <div class="widget">
-              <h5>How to find us</h5>
-              <address>
-				<i class="icon-home"></i> <strong>Lipscomb University</strong><br>
-				One University Park Drive<br>
-				Nashville, TN 37204
-				</address>
-              <p>
-                <i class="icon-phone"></i> (615) 966-5082 ext. 5082<br>
-                <i class="icon-envelope-alt"></i> lineupbarbershoppe@gmail.com
-              </p>
-            </div>
-            <div class="widget">
-              <ul class="social">
-                <li><a href="#" data-placement="bottom" title="Twitter"><i class="icon-twitter icon-square icon-32"></i></a></li>
-                <li><a href="#" data-placement="bottom" title="Facebook"><i class="icon-facebook icon-square icon-32"></i></a></li>
-                <li><a href="#" data-placement="bottom" title="Linkedin"><i class="icon-linkedin icon-square icon-32"></i></a></li>
-                <li><a href="#" data-placement="bottom" title="Pinterest"><i class="icon-pinterest icon-square icon-32"></i></a></li>
-                <li><a href="#" data-placement="bottom" title="Google plus"><i class="icon-google-plus icon-square icon-32"></i></a></li>
-              </ul>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="verybottom">
-        <div class="container">
-          <div class="row">
-            <div class="span6">
-              <p>
-                &copy; Lipscomb - All right reserved
-              </p>
-            </div>
-            <div class="span6">
-              <div class="pull-right">
-                <div class="credits">
-                  <!--
-                    All the links in the footer should remain intact.
-                    You can delete the links only if you purchased the pro version.
-                    Licensing information: https://bootstrapmade.com/license/
-                    Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/buy/?theme=Lumia
-                  -->
-                  Created by still the best group in SENG
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </footer>
+       <div class="container">
+         <div class="row">
+           <div class="span4">
+             <div class="widget">
+               <h5>How to find us</h5>
+               <address>
+         <i class="icon-home"></i> <strong>Lineup Barbershop</strong><br>
+         420 Lineup Ln.<br>
+         Nashville, TN 37204
+         </address>
+               <p>
+                 <i class="icon-phone"></i> (615) 966-5082 ext. 5082<br>
+                 <i class="icon-envelope-alt"></i> freshcutz@lineup.com
+               </p>
+             </div>
+             <div class="widget">
+               <ul class="social">
+                 <li><a href="#" data-placement="bottom" title="Twitter"><i class="icon-twitter icon-square icon-32"></i></a></li>
+                 <li><a href="#" data-placement="bottom" title="Facebook"><i class="icon-facebook icon-square icon-32"></i></a></li>
+                 <li><a href="#" data-placement="bottom" title="Linkedin"><i class="icon-linkedin icon-square icon-32"></i></a></li>
+                 <li><a href="#" data-placement="bottom" title="Pinterest"><i class="icon-pinterest icon-square icon-32"></i></a></li>
+                 <li><a href="#" data-placement="bottom" title="Google plus"><i class="icon-google-plus icon-square icon-32"></i></a></li>
+               </ul>
+             </div>
+           </div>
+         </div>
+       </div>
+       <div class="verybottom">
+         <div class="container">
+           <div class="row">
+             <div class="span6">
+               <p>
+                 &copy; LINEUP - All right reserved
+               </p>
+             </div>
+             <div class="span6">
+               <div class="pull-right">
+                 <div class="credits">
+                   <!--
+                     All the links in the footer should remain intact.
+                     You can delete the links only if you purchased the pro version.
+                     Licensing information: https://bootstrapmade.com/license/
+                     Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/buy/?theme=Lumia
+                   -->
+                   Created by the best group in SENG
+                 </div>
+               </div>
+             </div>
+           </div>
+         </div>
+       </div>
+     </footer>
 
   </div>
   <!-- end wrapper -->
